8026447336e8b9a1617c8e567075917aa6602fa2
snowwhitesheep <snw.wht.lk@gmail.com>
2023-05-12 16:21:51 +0200

    Variable and Expression done

--- src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
@@ -6,16 +6,57 @@ public class Expressions {
 
     public static Variable var(String name, int value) {
         //implement this method
-        return null;
+        return new Variable(name, value);
     }
 
     public static Expression val(int value) {
         //implement this method
-        return null;
+        Expression answer = new Expression() {
+            @Override
+            public int evaluate() {
+                return value;
+            }
+
+            @Override
+            public String toExpressionString() {
+                String valueS = String.valueOf(value);
+                if (value<0){
+                    valueS = "("+valueS+")";
+                }
+                return valueS;
+            }
+        };
+        return answer;
     }
+/*
+* Expression sum(Expression... members) - returns an Expression holding a sum of the given members.
+Consider that it is guaranteed that at least two members will be given each method call.
+Consider following methods' implementation  details:
+
+int evaluate() - returns the sum of evaluation result of all the given members.
+
+String toExpressionString() - returns a string representation of the given sum.
+Example: sum(val(1), val(2), val(3)).toExpressionString() results to (1 + 2 + 3).
+ */
 
     public static Expression sum(Expression... members){
         //implement this method
+        int sum = 0;
+        for(Expression member:members){
+            sum+=member.evaluate();
+        }
+        Expression answer = new Expression(){
+
+            @Override
+            public int evaluate() {
+                return sum;
+            }
+
+            @Override
+            public String toExpressionString() {
+                return null;
+            }
+        };
         return null;
     }
 
